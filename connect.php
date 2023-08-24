5f6e75f8603f54bb9f184d711b3e58e35d4c6ede
Abdullah Imran <abdullahimran999@gmail.com>
2023-08-24 22:51:32 +0500

    beginning of new version

--- connect.php
+++ connect.php
@@ -1,6 +1,10 @@
 <?php
 
-$con = mysqli_connect('localhost', 'root', '', 'courierbrosdb');
+// code for infinity free deployment
+$con = mysqli_connect('localhost', 'if0_34801428', 'NVhaDM13pfozc', 'if0_34801428_courierbrosdb');
+
+//local host config
+//$con = mysqli_connect('localhost', 'root', '', 'courierbrosdb');
 if (!$con) {
     die('cannot connected db');
 }
